f39f34702d2f779a5164b58a14823572832cbe65
hbw <1127621466@qq.com>
2022-08-15 17:47:01 +0800

    feat: 项目初始化，配置webpack

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// import './index.css';
+
+const App = () => {
+  return <div>hello 博文!!</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+const path = require('path');
+// 用模板生成一个Html文件
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// 打包前清理 打包目录
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); 
+// 命令行友好提示
+const friendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].bundle.js'
+  },
+  mode: 'development',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: 'asset/inline',
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: [
+              'style-loader',
+              {
+                loader: 'css-loader',
+                options: {
+                  importLoaders: 1,
+                },
+              },
+              'postcss-loader',
+            ],
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: '大文件切片上传',
+      template: path.resolve(__dirname, './public/index.html'),
+      filename: 'index.html',
+    }),
+    new CleanWebpackPlugin(),
+    new friendlyErrorsWebpackPlugin(),
+  ],
+  devServer: {
+    historyApiFallback: true,
+    static: path.join(__dirname, './dist'),
+    open: true,
+    hot: true,
+    port: 9797,
+  }
+}
