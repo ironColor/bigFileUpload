6a38023f87f23e623fe25285d712265f78e7db05
hbw <1127621466@qq.com>
2022-08-17 14:30:07 +0800

    feat: 完成文字渐入效果

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,12 +1,57 @@
-import React, { FC, memo,} from 'react';
+import { FC, memo, useRef,} from 'react';
 import AppBar from '@/layout/appBar'
+import styled from 'styled-components';
 
 export const Layout:FC<{ keyConfig?: string }> = memo(({ keyConfig }) => {
+  const textRef = useRef<HTMLDivElement>(null);
+
   return(
     <>
       <AppBar />
-      this is Layout
+      <BaseCard>
+        <TextWrap ref={textRef}>
+          <span>随风起，意难平</span>
+        </TextWrap>
+      </BaseCard>
     </>
     
   )
 })
+
+const BaseCard = styled.div`
+  position: sticky;
+  top: 0;
+  height: 50vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #000;
+`
+
+const TextWrap = styled.div`
+
+  width: 100vw;
+  height: 50vh;
+  line-height: 50vh;
+  text-align: center;
+  font-family: Helvetica;
+  margin: 0;
+  padding: 0;
+  font-size: 48px;
+  color: #fff;
+  letter-spacing: -0.3px;
+  
+  background-image: linear-gradient(75deg, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 1) 33.33%, rgba(255, 255, 255, 0) 66.67%, rgba(255, 255, 255, 0) 100%);
+  background-size: 300% 100%;
+  background-position-x: 100%;
+  
+  background-clip: text;
+  -webkit-background-clip: text;
+  color: transparent;
+
+  transition: 2s background-position-x ease-in-out;
+
+  &:hover {
+    background-position-x: 0%;
+  }
+`
